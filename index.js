1a14d09958a9e4ae361263ab48e357907d43c219
suisui65 <team-poodle@docomo.ne.jp>
2026-04-06 22:25:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 const { Client, GatewayIntentBits } = require("discord.js");
-require("dotenv").config();
+
+console.log("ENV TOKEN:", process.env.TOKEN);
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+  intents: [GatewayIntentBits.Guilds]
 });
 
 client.once("ready", () => {
-  console.log("Bot起動してるよ⭐");
-});
-
-client.on("messageCreate", (message) => {
-  if (message.author.bot) return;
-
-  if (message.content === "p@生成") {
-    message.reply("プレイヤー生成コマンド受け取った");
-  }
+  console.log("Bot起動成功");
 });
 
 client.login(process.env.TOKEN);
-
-console.log("ENV TOKEN:", process.env.TOKEN);
