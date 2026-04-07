ad2e93079a39e14ba9b4575870a5ad730502fce5
suisui65 <team-poodle@docomo.ne.jp>
2026-04-08 03:34:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+require("dotenv").config();
+
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
 client.once("ready", () => {
@@ -11,8 +17,8 @@ client.once("ready", () => {
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
 
-  if (message.content === "test") {
-    message.reply("動いてるよ！");
+  if (message.content === "ping") {
+    message.reply("pong!");
   }
 });
 
