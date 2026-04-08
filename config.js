af445edcdc3673a126cdcc9b9eabf365e57f1859
suisui65 <team-poodle@docomo.ne.jp>
2026-04-08 09:01:14 +0900

    Create config.js
    設定ファイル

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    JOBS: {
+        "剣士": { hp: 40, atk: 40, def: 30, spd: 10, luk: 10, mp: 20 },
+        "弓士": { hp: 30, atk: 50, def: 30, spd: 15, luk: 15, mp: 20 },
+        "魔術師": { hp: 25, atk: 60, def: 25, spd: 8, luk: 12, mp: 20 },
+        "タンク": { hp: 50, atk: 20, def: 40, spd: 5, luk: 10, mp: 20 }
+    },
+    BOSS_DATA: {
+        name: "イデア・ジェネシス",
+        max_hp: 1200, atk: 10, def: 20, spd: 30, luk: 20,
+        image: "https://cdn.discordapp.com/attachments/1491215421146140823/1491215491669168168/1775603874321.jpg"
+    }
+};
