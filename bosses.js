427f7011b090718adb4fd123e32d358dded8ed7e
suisui65 <team-poodle@docomo.ne.jp>
2026-04-11 16:17:54 +0900

    Update bosses.js

--- bosses.js
+++ bosses.js
@@ -1,12 +1,15 @@
+/**
+ * 【編集ガイド】
+ * mp: スキルを使うためのリソース
+ * mp_regen: 毎ターン(全員の一巡後)に回復する量
+ */
 module.exports = {
     "dragon_1": {
-        name: "古の黒龍", hp: 1000, atk: 30, def: 20, spd: 8, luk: 10,
+        name: "古の黒龍",
+        hp: 1000, max_hp: 1000,
+        atk: 30, def: 20, spd: 8, luk: 10,
+        mp: 100, max_mp: 100, mp_regen: 20,
         rank: 1, exp_reward: 50, money_reward: 1000,
         imageUrl: "https://i.imgur.com/Aw2zG6K.png"
-    },
-    "slime_king": {
-        name: "キングスライム", hp: 300, atk: 15, def: 5, spd: 5, luk: 5,
-        rank: 1, exp_reward: 15, money_reward: 300,
-        imageUrl: "https://i.imgur.com/Bw1zG5J.png"
     }
 };
