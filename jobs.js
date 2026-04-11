11ce0dca1c46468eb1860eb9206af231e09c836f
suisui65 <team-poodle@docomo.ne.jp>
2026-04-11 14:15:31 +0900

    Create jobs.js

---
+++ jobs.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "剣士": {
+        hp: 120, atk: 25, def: 15, spd: 12, luk: 10,
+        pos: "前衛", weaponType: "剣",
+        imageUrl: "https://i.imgur.com/8Xb3A5O.png" // 好きなURLに変えてね
+    },
+    "弓士": {
+        hp: 80, atk: 18, def: 8, spd: 15, luk: 10,
+        pos: "後衛", weaponType: "弓",
+        imageUrl: "https://i.imgur.com/Yw4zG8M.png"
+    },
+    "魔術師": {
+        hp: 70, atk: 40, def: 5, spd: 10, luk: 15,
+        pos: "中衛", weaponType: "杖",
+        imageUrl: "https://i.imgur.com/vH4V0fN.png"
+    },
+    "タンク": {
+        hp: 180, atk: 10, def: 25, spd: 5, luk: 5,
+        pos: "前衛", weaponType: "盾",
+        imageUrl: "https://i.imgur.com/UfXb4zR.png"
+    },
+    "商人": {
+        hp: 90, atk: 12, def: 10, spd: 10, luk: 20,
+        pos: "中衛", weaponType: "カバン",
+        imageUrl: "https://i.imgur.com/wF9YkM0.png"
+    }
+};
