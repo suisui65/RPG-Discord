ede1f74c8863469ea43e92273b1b9ab8dc820f39
suisui65 <team-poodle@docomo.ne.jp>
2026-04-11 14:18:22 +0900

    Create bosses.js

---
+++ bosses.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "dragon_1": {
+        name: "古の黒龍", hp: 1000, atk: 30, def: 20, spd: 8, luk: 10,
+        rank: 1, exp_reward: 50, money_reward: 1000,
+        imageUrl: "https://i.imgur.com/Aw2zG6K.png"
+    },
+    "slime_king": {
+        name: "キングスライム", hp: 300, atk: 15, def: 5, spd: 5, luk: 5,
+        rank: 1, exp_reward: 15, money_reward: 300,
+        imageUrl: "https://i.imgur.com/Bw1zG5J.png"
+    }
+};
