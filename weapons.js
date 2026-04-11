3ecaf9b9903073e91cf16d2c3c76a5b5c3e70621
suisui65 <team-poodle@docomo.ne.jp>
2026-04-11 14:15:58 +0900

    Create weapons.js

---
+++ weapons.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "初期の剣": { type: "剣", atk_plus: 5, price: 0, imageUrl: "https://i.imgur.com/Xw8zG9N.png" },
+    "鋼鉄の剣": { type: "剣", atk_plus: 15, price: 1000, imageUrl: "https://i.imgur.com/Xw8zG9N.png" },
+    "木の弓": { type: "弓", atk_plus: 8, price: 300, imageUrl: "https://i.imgur.com/Yw4zG8M.png" }
+};
