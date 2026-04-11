2c7c540896f22a94c49ad819a3f0c5312c9d8a67
suisui65 <team-poodle@docomo.ne.jp>
2026-04-11 18:22:10 +0900

    Update bosses.js

--- bosses.js
+++ bosses.js
@@ -5,11 +5,11 @@
  */
 module.exports = {
     "dragon_1": {
-        name: "古の黒龍",
-        hp: 1000, max_hp: 1000,
-        atk: 30, def: 20, spd: 8, luk: 10,
-        mp: 100, max_mp: 100, mp_regen: 20,
+        name: "イデア・ジェネシス",
+        hp: 1100, max_hp: 1100,
+        atk: 20, def: 20, spd: 8, luk: 10,
+        mp: 120, max_mp: 120, mp_regen: 20,
         rank: 1, exp_reward: 50, money_reward: 1000,
-        imageUrl: "https://i.imgur.com/Aw2zG6K.png"
+        imageUrl: "https://cdn.discordapp.com/attachments/1491215421146140823/1491215491669168168/1775603874321.jpg"
     }
 };
